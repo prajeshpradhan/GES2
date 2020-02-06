37d9ca4f1bae590d8192e9a230cd1eb587f4ea88
prajeshpradhan <prajeshpradhan2013@gmail.com>
2020-02-06 13:59:49 +0545

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,35 +23,7 @@
 
 </head>
 <body>
-<div class="header-container">
-    <div id="Header" class="header">
-        <div class="logo-container">
-            <img src="{{asset('images/GES%20Logo%20New.png')}}" class="img-responsive">
-        </div>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact Us</a></li>
-{{--            TEMP LOGOUT BUTTON--}}
-            @auth
-                <li><a href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
-            @endauth
-
-        </ul>
-    </div>
-</div>
-
+@include('layouts.header')
 <div class="main-banner">
     <div class="main-banner-txt">
         <h1>Place Your Tagline Here</h1>
